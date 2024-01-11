978f4a68c20f1d5337d0b0a49d7e0c052cdf01c3
Rav <raheemhakeem25@gmail.com>
2024-01-11 03:21:55 +0100

    Completed styling of Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,46 +7,62 @@ import BestSelling from "../components/BestSelling";
 import SecondAD from "../components/SecondAD/SecondAD";
 import ExploreProducts from "../components/ExploreProducts";
 import NewArrival from "../components/NewArrival";
+import Assurance from "../components/Assurance";
+import { IoMdArrowUp } from "react-icons/io";
 import { styles } from "../styles";
 
 const Home = () => {
   return (
-    <div className={`${styles.marginX}`}>
-      {/* section 1 */}
-      <div className="flex justify-between">
-        <div className="">
-          <Items />
+    <>
+      <div className={`${styles.marginX}`}>
+        {/* section 1 */}
+        <div className="flex justify-between">
+          <div className="">
+            <Items />
+          </div>
+          <div className="w-[1px] mx-4  bg-slate-300 "></div>
+          <div className="sectionimg flex-grow">
+            <HeaderAD />
+          </div>
         </div>
-        <div className="w-[1px] mx-4  bg-slate-300 "></div>
-        <div className="sectionimg flex-grow">
-          <HeaderAD />
+        {/* section 2 Flash Sales */}
+        <div className=" mt-[138px]">
+          <FlashSales />
+        </div>
+        {/* section 3 Categories */}
+        <div className="mt-[80px]">
+          <Categories />
+        </div>
+        {/* section 4 Best selling */}
+        <div className="mt-[70px]">
+          <BestSelling />
+        </div>
+        {/* section 5 secondAD */}
+        <div className="mt-[140px]">
+          <SecondAD />
+        </div>
+        {/* section 6 Exploring Products */}
+        <div className="mt-[70px]">
+          <ExploreProducts />
+        </div>
+        {/* section 7 new arrival */}
+        <div className="mt-[80px]">
+          <NewArrival />
         </div>
       </div>
-      {/* section 2 Flash Sales */}
-      <div className=" mt-[138px]">
-        <FlashSales />
+      {/* section 8 assurance */}
+      <div className="mt-[140px] mx-[249px]">
+        <Assurance />
       </div>
-      {/* section 3 Categories */}
-      <div className="mt-[80px]">
-        <Categories />
+      {/* back to top button */}
+      <div className={`${styles.marginX} mt-[62px] mb-8 flex justify-end`}>
+        <div className="flex">
+          <button className="w-[26px] h-[26px] text-xl bg-slate-400 rounded-full flex justify-center items-center">
+            <IoMdArrowUp />
+          </button>
+        </div>
       </div>
-      {/* section 4 Best selling */}
-      <div className="mt-[70px]">
-        <BestSelling />
-      </div>
-      {/* section 5 secondAD */}
-      <div className="mt-[140px]">
-        <SecondAD />
-      </div>
-      {/* section 6 Exploring Products */}
-      <div className="mt-[70px]">
-        <ExploreProducts />
-      </div>
-      {/* section 7 new arrival */}
-      <div className="mt-[80px]">
-        <NewArrival />
-      </div>
-    </div>
+    </>
   );
 };
 
