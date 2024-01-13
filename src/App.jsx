0a10c3227170a4fc0f235538db9b14e60192a984
Rav <raheemhakeem25@gmail.com>
2024-01-14 00:31:29 +0100

    Completed about page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import TopHeader from "./components/TopHeader";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
       <Footer />
     </div>
