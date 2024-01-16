1ed0065277b180bb54124eb872fa541d3aa561d2
Rav <raheemhakeem25@gmail.com>
2024-01-16 23:24:50 +0100

    Added functionality to scroll-up button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,12 @@ import { IoMdArrowUp } from "react-icons/io";
 import { styles } from "../styles";
 
 const Home = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    })
+  }
   return (
     <>
       <div className={`${styles.marginX}`}>
@@ -56,8 +62,8 @@ const Home = () => {
       </div>
       {/* back to top button */}
       <div className={`${styles.marginX} mt-[62px] mb-8 flex justify-end`}>
-        <div className="flex">
-          <button className="w-[26px] h-[26px] text-xl bg-slate-400 rounded-full flex justify-center items-center">
+        <div className="flex" >
+          <button className="w-[26px] h-[26px] text-xl bg-slate-400 rounded-full flex justify-center items-center" onClick={scrollToTop}>
             <IoMdArrowUp />
           </button>
         </div>
